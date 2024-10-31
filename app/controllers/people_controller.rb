74ee2617bd407ed8dee2fe19885287cd976f2689
Rylee Jennings <rjennings2@academic.rrc.ca>
2024-10-30 20:20:54 -0500

    4.1 and 4.2 Searches

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,14 @@
 class PeopleController < ApplicationController
   def index
+    @people = Person.all
+
+    if params[:query].present?
+      @people = Person.where('name ILIKE ?', "%#{params[:query]}%")
+    end
+
+    if params[:planet_id].present?
+      @people = @people.where(planet_id: params[:planet_id])
+    end
   end
 
   def show
