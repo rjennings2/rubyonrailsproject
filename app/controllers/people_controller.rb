5ac2d0415957dfccdfa49020464bd90f4d94f37c
Rylee Jennings <rjennings2@academic.rrc.ca>
2024-10-30 20:46:39 -0500

    Search Error Fixes

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,18 +1,16 @@
 class PeopleController < ApplicationController
   def index
-    @people = Person.all
-
+    @people = Person.page(params[:page]).per(10)
     if params[:query].present?
-      @people = Person.where('name ILIKE ?', "%#{params[:query]}%")
+      @people = @people.where('name ILIKE ?', "%#{params[:query]}%")
     end
 
     if params[:planet_id].present?
       @people = @people.where(planet_id: params[:planet_id])
     end
-
-    @people = @people.page(params[:page]).per(10)
   end
 
   def show
+    @person = Person.find(params[:id])
   end
 end
